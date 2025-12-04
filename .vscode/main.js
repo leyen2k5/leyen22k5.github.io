7637337a969f7b3255129f037417adf49bcb912c
leyen2k5 <23070688@vnu.edu.vn>
2025-12-04 15:23:10 +0700

    .

---
+++ .vscode/main.js
@@ -0,0 +1,35 @@
+
+// alert là một hàm để hiển thị hộp thoại thông báo trên trình duyệt, kèm theo một thông điệp và một nút "OK"
+// alert confirm va promt hien thi len cua so hop thoai
+var fullName = 'Le Thi Yen';
+var age = 20;
+alert(fullName);
+/* var dùng để khai báo biến */
+console.log(fullName);
+// in tại console
+confirm('Xac nhan ban du tuoi!');
+prompt('Xac nhan ban du tuoi!');
+// prompt hiện hộp thoại
+setTimeout(function () {
+    alert('thong bao')
+}, 1000)
+//cho doan code chay sau khoang thoi gian, chay 1 lan
+setInterval(function () {
+    console.log('Day la log' + Math.random)
+}, 1000)
+//doan code thuc hien lien tuc lap di lap lai
+/**
+ * giới thiệu về toán t rtrong Java
+ 1. Toán tử số học - Arithmetic
+ var a = 1 * 2;
+ console.log(a)
+ 2. Toán tử gán - Assignment
+ var fullName = 'Le Thi Yen';
+ 3. Toán tử so sánh - Comparison
+ var a = 1;
+ var b = 2;
+ if (a > b){
+ alert('Dung');
+ }
+ 4. Toán tử logic - Logical
+ */
